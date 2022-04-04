520a479d21526edfe4b6a9dcc495077f04b081ec
darthradek <radoslawhaller@gmail.com>
2022-04-04 23:46:16 +0200

    Implement scripts

---
+++ scripts/pull-repos.sh
@@ -0,0 +1,1 @@
+for d in ../Kikaku/*/ ; do (cd "$d" && git pull && echo "$d"); done
---
+++ scripts/setup-project.sh
@@ -0,0 +1,13 @@
+cd ../Kikaku
+
+// Pull API
+git clone git@github.com:darthradek/Kikaku-API.git
+cd Kikaku-API
+git checkout development
+cd ..
+
+// Pull Web
+git clone git@github.com:darthradek/Kikaku-Web.git
+cd Kikaku-Web
+git checkout development
+cd ..
